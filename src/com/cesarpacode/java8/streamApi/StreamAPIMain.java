419305df13f841d6bb8c94207a4fc701374e1f4f
unknown <cesar.1023@hotmail.com>
2021-07-20 19:09:30 -0500

    J8I stream of Integers, also reducing method complexity

--- src/com/cesarpacode/java8/streamApi/StreamAPIMain.java
+++ src/com/cesarpacode/java8/streamApi/StreamAPIMain.java
@@ -22,15 +22,10 @@ public class StreamAPIMain {
         System.out.println(values.stream().map(i -> i * 2).reduce(0, (c, e) -> c + e));
 
         // Step by Step
-        BinaryOperator<Integer> b = new BinaryOperator<Integer>() {
-            public Integer apply(Integer i, Integer j) {
-                return i + j;
-            }
-        };
-        Stream s1 = values.stream();
+        Stream<Integer> s1 = values.stream();
         Function<Integer, Integer> f = (i -> i * 2);
-        Stream s2 = s1.map(f);
-        Integer result = (Integer) s2.reduce(0, b);
+        Stream<Integer> s2 = s1.map(i -> i * 2);
+        Integer result = s2.reduce(0, (i, j) -> i + j);
         System.out.println(result);
     }
 
